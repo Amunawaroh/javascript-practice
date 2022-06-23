7c3fa431127b5d5027c9d0d626ebfc75d5506aa2
Amunawaroh <dammykay435@gmail.com>
2022-06-23 18:59:41 +0100

    about-me

---
+++ index.js
@@ -0,0 +1,7 @@
+const myName = 'Munawaroh Adedamola';
+const myHeight = '178cm';
+const myCountry = 'Nigeria';
+
+const aboutMe = (`My name is ${myName}, I'm ${myHeight} tall and I'm a citizen of ${myCountry}`);
+
+alert(aboutMe)
